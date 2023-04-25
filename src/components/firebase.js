92836d671e22db9daf9efe580d3614e5a630bcda
Shlomi Yahbes <99732661+syahbes@users.noreply.github.com>
2023-04-25 09:32:02 +0300

    Update firebase.js

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -4,12 +4,7 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC1Dg0NF-CFsvuO-XvD9OIAuwRDoOy1IXc",
-  authDomain: "todo-v2-11bf6.firebaseapp.com",
-  projectId: "todo-v2-11bf6",
-  storageBucket: "todo-v2-11bf6.appspot.com",
-  messagingSenderId: "639354247966",
-  appId: "1:639354247966:web:0bae51b55ff8d771a5470f"
+
 };
 
 // Initialize Firebase
